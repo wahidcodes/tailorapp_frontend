86c7ba2b7c72def6d7d7888c5edfa8279a682b87
wahid_md <amwahid2004@gmail.com>
2024-07-18 11:08:07 +0530

    footer height modified

--- src/comps/DashBoard.tsx
+++ src/comps/DashBoard.tsx
@@ -156,7 +156,7 @@ const DashBoard = () => {
         </div>
 
         {/*                                              !--------FOOTER---------!                                                */}
-        <div style={{width:'100%',backgroundColor:'#00000f',color:"white",height:'50px',display:'flex',justifyContent:'center'}}>
+        <div style={{width:'100%',backgroundColor:'#00000f',color:"white",height:'8vh',display:'flex',justifyContent:'center'}}>
               <div style={{margin:15,fontFamily:'sans-serif'}}>
                   © Copyright 2024. Developed and maintained by <a href="linkedin.com/in/mohamed-wahid-7817bb239">Mohamed Wahid</a>
               </div>
